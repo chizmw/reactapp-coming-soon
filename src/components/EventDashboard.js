8d5b63a952452aedfa98b70ae18d415f411f1c8d
Chisel <chisel@malik-wright.uk>
2025-02-21 10:28:52 +0000

    feat: step towards in-page tag filtering

--- src/components/EventDashboard.js
+++ src/components/EventDashboard.js
@@ -1,81 +1,95 @@
 'use client';
 
-import PropTypes from 'prop-types';
-
 import { useState, useEffect } from 'react';
 import { formatDistanceToNow, parseISO } from 'date-fns';
-import { rrulestr } from 'rrule';
+import { RRule } from 'rrule';
+import PropTypes from 'prop-types';
 
-const EventDashboard = ({ filterTag = '' }) => {
+const formatDate = (date) => {
+  const options = { day: '2-digit', month: 'long', year: 'numeric' };
+  return date.toLocaleDateString(undefined, options);
+};
+
+const EventDashboard = ({ selectedTags = [] }) => {
   const [events, setEvents] = useState([]);
+  const [fetchedData, setFetchedData] = useState([]);
+  const [allTags, setAllTags] = useState([]);
 
+  // Fetch the JSON data
   useEffect(() => {
     fetch('/events.json')
       .then((response) => response.json())
       .then((data) => {
-        const now = new Date();
-        const eventsWithNextOccurrence = data
-          .filter((event) => !filterTag || event.tags.includes(filterTag))
-          .map((event) => {
-            if (event.rrule) {
-              console.info('Found rrule');
-              try {
-                const rule = rrulestr(`DTSTART:${event.start}\n${event.rrule}`);
-                const startDate = parseISO(event.start);
-                console.log(rule);
-                console.log(startDate);
+        setFetchedData(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching events:', error);
+      });
+  }, []);
 
-                // Calculate the next occurrence after the current date
-                const nextOccurrence = rule.after(now, true);
-                console.info(nextOccurrence);
-                console.info(
-                  `Event: ${event.summary}, Next Occurrence: ${new Date(
-                    nextOccurrence
-                  ).toLocaleString()}`
-                );
+  // Extract unique tags
+  useEffect(() => {
+    if (fetchedData.length > 0) {
+      const tagsSet = new Set(fetchedData.flatMap((event) => event.tags || []));
+      setAllTags(Array.from(tagsSet));
+    }
+  }, [fetchedData]);
 
-                return {
-                  ...event,
-                  nextOccurrence: nextOccurrence || startDate,
-                };
-              } catch (error) {
-                console.error(
-                  `Invalid recurrence rule for event: ${event.summary}`,
-                  error
-                );
-                return { ...event, nextOccurrence: parseISO(event.start) };
-              }
-            } else {
-              // One-time event
+  // Process events
+  useEffect(() => {
+    if (fetchedData.length > 0 && allTags.length > 0) {
+      console.log('allTags:', allTags);
+      const now = new Date();
+      const eventsWithNextOccurrence = fetchedData
+        .filter((event) =>
+          selectedTags.length > 0
+            ? selectedTags.some((tag) => event.tags.includes(tag))
+            : true
+        )
+        .map((event) => {
+          if (event.recurrence) {
+            try {
+              const ruleString = event.recurrence.trim();
+              const rule = new RRule(ruleString);
+              const startDate = parseISO(event.start);
+              rule.options = { dtstart: startDate, tzid: event.timezone };
+
+              const nextOccurrence = rule.after(now, true);
+              const nextOccurrenceDate = nextOccurrence || startDate;
+
+              return { ...event, nextOccurrence: nextOccurrenceDate };
+            } catch (error) {
+              console.error(
+                `Invalid recurrence rule for event: ${event.summary}`,
+                error
+              );
               return { ...event, nextOccurrence: parseISO(event.start) };
             }
-          });
+          } else {
+            const nextOccurrenceDate = parseISO(event.start);
+            return { ...event, nextOccurrence: nextOccurrenceDate };
+          }
+        });
 
-        // Sort events by next occurrence
-        const sortedEvents = eventsWithNextOccurrence.sort(
-          (a, b) => new Date(a.nextOccurrence) - new Date(b.nextOccurrence)
-        );
+      const sortedEvents = eventsWithNextOccurrence.sort(
+        (a, b) => new Date(a.nextOccurrence) - new Date(b.nextOccurrence)
+      );
 
-        setEvents(sortedEvents);
-      });
-  }, [filterTag]);
-
-  const formatDate = (date) => {
-    const options = { day: '2-digit', month: 'long', year: 'numeric' };
-    return date.toLocaleDateString(undefined, options);
-  };
+      setEvents(sortedEvents);
+    }
+  }, [fetchedData, allTags]);
 
   return (
     <div className="event-dashboard">
       {events.map((event, index) => (
-        <div key={index} className="event-tile">
+        <div key={event.summary} className="event-tile">
           <img src={event.image} alt={event.summary} />
           <div className="content">
             <h2>{event.summary}</h2>
             <h3>{formatDate(event.nextOccurrence)}</h3>
-            <h4>
+            <h3>
               {formatDistanceToNow(event.nextOccurrence, { addSuffix: true })}
-            </h4>
+            </h3>
           </div>
         </div>
       ))}
@@ -84,7 +98,7 @@ const EventDashboard = ({ filterTag = '' }) => {
 };
 
 EventDashboard.propTypes = {
-  filterTag: PropTypes.string,
+  selectedTags: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default EventDashboard;
