2e661e5cdb4bbcbf6ed86cdbd530f47d6f425579
Chisel <chisel@malik-wright.uk>
2025-02-23 15:54:32 +0000

    feat: add basic error handling for failed even loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,29 @@ export default function Home() {
     const savedTags = localStorage.getItem('selectedTags');
     return savedTags ? JSON.parse(savedTags) : [];
   });
+  const [error, setError] = useState<string | null>(null);
+  const [pageTitle, setPageTitle] = useState<string>('Coming Soon');
 
   // Fetch the JSON data
   useEffect(() => {
     fetch('/events.json')
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch events: ' + response.statusText);
+        }
+        return response.json();
+      })
       .then((data) => {
-        // add 'guid' property to each event
+        // Add 'guid' property to each event
         data.forEach((event: EventItem) => {
           event.guid = crypto.randomUUID();
         });
         setLoadedJsonData(data);
       })
       .catch((error) => {
-        console.error('Error fetching events:', error);
+        console.error('Caught: Error fetching events:', error);
+        setPageTitle('Error');
+        setError('Failed to load events. Please try again later.');
       });
   }, []);
 
@@ -39,9 +48,18 @@ export default function Home() {
     }
   }, [loadedJsonData]);
 
+  if (error) {
+    return (
+      <div>
+        <h1>{pageTitle}</h1>
+        <div className="error-message">{error}</div>;
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Coming Soon</h1>
+      <h1>{pageTitle}</h1>
       {loadedJsonData ? (
         <EventDashboard
           jsonData={loadedJsonData}
