419abffa10ffb7362e4c0b6c7abe9a1c7c53811c
Chisel <chisel@malik-wright.uk>
2025-02-23 20:48:45 +0000

    chore: The Next.js plugin was not detected in your ESLint configuration
    > See https://nextjs.org/docs/app/api-reference/config/eslint#migrating-existing-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,19 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import pluginNext from '@next/eslint-plugin-next';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     settings: { react: { version: 'detect' } },
+    plugins: {
+      '@next/next': pluginNext,
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+    },
   },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
